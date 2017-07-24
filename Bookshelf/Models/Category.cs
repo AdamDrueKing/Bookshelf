4ad6ad18e094e5ad10713d815643c746ec95e58c
Adam King <adam81king@gmail.com>
2017-07-24 14:17:36 -0400

    added book and category models

---
+++ Bookshelf/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bookshelf.Models
+{
+    public class Category
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Book> Books { get; set; }
+        //ICollection is a Navigation Property
+
+    }
+}
